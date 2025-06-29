d3f885d3c75585b170580591cc344799ac22d36b
Articte <articate@gmail.com>
2025-06-29 13:48:05 +0200

    Add Contenful integration, enviornment variables, and test it

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    async function fetchEntries() {
+      try {
+        const response = await client.getEntries({
+          content_type: "accordion",
+          include: 2,
+        });
+        console.log("Contentful data:", response);
+      } catch (error) {
+        console.error("Error fetching from Contentful:", error);
+      }
+    }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    fetchEntries();
+  }, []);
+
+  return <span>Check console</span>;
 }
 
-export default App
+export default App;
